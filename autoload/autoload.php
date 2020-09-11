f6b9942d98ebcd89c9c19c7ed7eaa402b8a88cce
KingNNT <Dev.KingNNT@gmail.com>
2020-09-11 17:07:57 +0700

    Update

--- autoload/autoload.php
+++ autoload/autoload.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
 
-require_once("./vendor/autoload.php");
 require_once('./config/config.php');
+require_once('./config/Database.php');
+require_once("./vendor/autoload.php");
