c945be66a9427486d9ae061bd285fa2d256fc153
KingNNT <Dev.KingNNT@gmail.com>
2020-09-17 16:43:51 +0700

    Update

---
+++ models/jobModel.php
@@ -0,0 +1,25 @@
+<?php
+    require_once "./autoload/autoload.php";
+    class jobModel
+    {
+        public function __construct()
+        {
+        }
+        
+        /* CRUD */
+        public static function create()
+        {
+        }
+        public static function read()
+        {
+        }
+        public static function update()
+        {
+        }
+        public static function delete()
+        {
+        }
+        public static function find()
+        {
+        }
+    }
